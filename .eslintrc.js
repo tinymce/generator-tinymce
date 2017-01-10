bafcafd610fa5fdd174007c8302d52feabd7c455
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2017-01-10 11:17:34 +0100

    change eslint, fiddling aroung

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "standard",
+    "env": {
+      "mocha": true
+    },
+    "plugins": [
+        "standard",
+        "promise"
+    ]
+};
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,52 @@
-'use strict';
-var Generator = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+'use strict'
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
+const path = require('path')
+const _ = require('lodash')
 
 module.exports = Generator.extend({
-  prompting: function () {
+  prompting () {
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the delightful ' + chalk.red('generator-tiny-plugin') + ' generator!'
-    ));
+    ))
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        name: 'name',
+        message: 'Plugin name:',
+        default: path.basename(process.cwd()),
+        filter: _.kebabCase,
+        validate: str => str.length > 0
+      }
+    ]
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
+      this.props = props
+    }.bind(this))
   },
 
-  writing: function () {
+  default () {
+    console.log('default')
+    this.composeWith(require.resolve('generator-license/app'), {
+      name: 'Some name',
+      email: 'mail@internet.com',
+      website: 'www.web.se'
+    })
+  },
+
+  writing () {
+    console.log('writing')
+    const {name} = this.props
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+      this.destinationPath(name + 'dummyfile.txt')
+    )
   },
 
-  install: function () {
-    this.installDependencies();
+  install () {
+    this.installDependencies()
   }
-});
+})
