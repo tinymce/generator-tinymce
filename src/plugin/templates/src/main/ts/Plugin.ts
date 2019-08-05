461ff0da8fad6cf1db9199e5b2a747d6279bd706
Joshua Haines <josh.j.haines@gmail.com>
2019-08-05 14:42:56 +1000

    deprecation-fixes: fix deprecated UI registration

--- src/plugin/templates/src/main/ts/Plugin.ts
+++ src/plugin/templates/src/main/ts/Plugin.ts
@@ -1,10 +1,9 @@
 declare const tinymce: any;
 
 const setup = (editor, url) => {
-  editor.addButton('<%= name %>', {
+  editor.ui.registry.addButton('<%= name %>', {
     text: '<%= name %> button',
-    icon: false,
-    onclick: () => {
+    onAction: () => {
       // tslint:disable-next-line:no-console
       editor.setContent('<p>content added from <%= name %></p>');
     }
