0ca9d9a95107bfd892e11929e58b4474bfe3cf4e
Dylan Just <dylan.just@tiny.cloud>
2020-04-20 17:04:06 +1000

    Update the dependencies within the template.

--- src/plugin/templates/src/test/ts/atomic/AddTwoTest.ts
+++ src/plugin/templates/src/test/ts/atomic/AddTwoTest.ts
@@ -1,7 +1,7 @@
-import { UnitTest, assert } from '@ephox/bedrock';
+import { UnitTest, Assert } from '@ephox/bedrock-client';
 import { addTwo } from '../../../main/ts/core/AddTwo';
 
 // This is an example of an atomic test, that is a test of some functionality separated from the editor.
 UnitTest.test('atomic.AddTwoTest', () => {
-  assert.eq(3, addTwo(1), '1 + 2 = 3, hopefully');
+  Assert.eq('1 + 2 = 3, hopefully', 3, addTwo(1));
 });
