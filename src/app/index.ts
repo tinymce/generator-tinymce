0d00cee641c80586f362dcdc046438f9d4ea20b3
Dylan Just <dylan.just@tiny.cloud>
2020-04-20 15:09:33 +1000

    Update dependencies

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,5 @@
 import * as Generator from 'yeoman-generator';
-import chalk from 'chalk';
+import * as chalk from 'chalk';
 import * as yosay from 'yosay';
 import * as path from 'path';
 import { kebabCase } from 'lodash';
